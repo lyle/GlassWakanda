13593814a45aab6309d4022ce6d6bb4968c18175
Lyle Troxell <lyle@troxell.com>
2013-07-22 14:28:47 -0700

    People view page

--- Glass/WebFolder/People.waPage/scripts/index.js
+++ Glass/WebFolder/People.waPage/scripts/index.js
@@ -2,10 +2,24 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var matrix1 = {};	// @matrix
 	var person0Event = {};	// @dataSource
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	matrix1.onChildrenDraw = function matrix1_onChildrenDraw (event)// @startlock
+	{// @endlock
+		//console.log(event);
+		var img = $(event.htmlObject.selector + ' img');
+		if (event.source.picture){
+			img[0].src = event.source.picture;
+			//imgage.show();
+		}else{
+			//imgage.hide();
+		}
+		
+	};// @lock
 
 	person0Event.onCurrentElementChange = function person0Event_onCurrentElementChange (event)// @startlock
 	{// @endlock
@@ -20,6 +34,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("matrix1", "onChildrenDraw", matrix1.onChildrenDraw, "WAF");
 	WAF.addListener("person0", "onCurrentElementChange", person0Event.onCurrentElementChange, "WAF");
 // @endregion
 };// @endlock
