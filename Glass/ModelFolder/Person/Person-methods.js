2897f3225211239265d8ca3ac57705c3c9e6e3cc
Lyle Troxell <lyle@troxell.com>
2013-07-23 14:29:19 -0700

    hide email

--- Glass/ModelFolder/Person/Person-methods.js
+++ Glass/ModelFolder/Person/Person-methods.js
@@ -1,1 +1,30 @@
-model.Person.methods.getCurrentPerson = function(){	var user = currentSession().user;//	return user;	if(user.ID === "00000000000000000000000000000000" || ! user.fullName){		//default user - no one is logged in		//throw new UserException("Param type error", 1);		return {error: 100, errorMessage: 'You are not Logged in babay!'};	}else{//		return user;		return ds.Person({email:user.name});	}};model.Person.methods.getCurrentPerson.scope ="public";
+model.Person.methods.getCurrentPerson = function(){
+	var user = currentSession().user;
+//	return user;
+	if(user.ID === "00000000000000000000000000000000" || ! user.fullName){
+		//default user - no one is logged in
+		//throw new UserException("Param type error", 1);
+		return {error: 100, errorMessage: 'You are not Logged in babay!'};
+	}else{
+
+//		return user;
+		return ds.Person({ID:user.ID});
+	}
+};
+
+model.Person.events.onLoad=function(attributeName){
+	if (currentSession().user.ID != this.ID) {
+		try {
+			currentSession().checkPermission('RealAdmin');
+		}catch(e){
+			this.email = "****";
+		}
+	}
+};
+
+
+model.Person.methods.getCurrentPerson.scope ="public";
+
+
+
+
