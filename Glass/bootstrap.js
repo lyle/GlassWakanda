3e7df1de49b28dd851fafc2b3752b3e6a58af28b
Lyle Troxell <lyle@troxell.com>
2013-07-23 10:01:21 -0700

    remove debugging handlers

--- Glass/bootstrap.js
+++ Glass/bootstrap.js
@@ -1,6 +1,6 @@
 directory.setLoginListener("googleListener", "Admin");
 
 addHttpRequestHandler("^/login", "Scripts/ReceiveOAuth2.js",  "login");
-addHttpRequestHandler("^/GoogleMirrorList", "Scripts/GoogleMirror.js",  "list");
+//addHttpRequestHandler("^/GoogleMirrorList", "Scripts/GoogleMirror.js",  "list");
 
-addHttpRequestHandler("^/GoogleMirrorPost", "Scripts/GoogleMirror.js",  "post");
+//addHttpRequestHandler("^/GoogleMirrorPost", "Scripts/GoogleMirror.js",  "post");
