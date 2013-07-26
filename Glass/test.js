63dabff11f6387b3781c99b481c31f2d9d5b4d51
Lyle Troxell <lyle@troxell.com>
2013-07-25 20:38:12 -0400

    better all peeps?

--- Glass/test.js
+++ Glass/test.js
@@ -1,6 +1,21 @@
-﻿__dirname = "/User/ltroxell/Code/wakanda/Glass/Glass/";
-var gl= require('googleapis');
-//var gl= require('querystring');
-//gl.stringify({"dd":'sd'});
-typeof gl;
-//ds.Person.all().length;
+﻿var people;
+peopleCollection = ds.Person.query("emailHash = null");
+//peopleCollection = ds.Person.all();
+peopleCollection;
+
+
+
+
+
+
+
+
+
+for(var i = 0; i < peopleCollection.length; i++)
+{
+	var theEnt = peopleCollection[i];
+	if (theEnt.ID = '23D966F360BB44F1A788FA9B177C6916'){
+		//theEnt.created_at = null;
+		//theEnt.save();
+	}
+}
