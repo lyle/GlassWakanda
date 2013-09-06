01d64fbd2dd1ab5022cda83eb725b6f66335964c
Lyle Troxell <lyle@troxell.com>
2013-09-06 16:20:01 -0700

    add test file -

--- Glass/test.js
+++ Glass/test.js
@@ -2,13 +2,46 @@
 //peopleCollection = ds.Person.query("emailHash = null");
 //peopleCollection = ds.Person.all();
 //peopleCollection;
+var lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
+lyle = ds.Person('39CCB5927E9446D9B181EB4336FC6B30');
 
-lyle = ds.Person.find('firstName=Lyle');
-
+	currentSession().promoteWith('Administrator');
 lyle;
+//ds.Person.find('firstName=Xiang');
+//googlID = ds.GoogleAccess('101982392616981120013');//Lyle
 
-googlID = ds.GoogleAccess('101982392616981120013');
+//var Mirror = require('MirrorAPI').Mirror;
+//var mir = new Mirror(googlID);
+//var mir = new Mirror(googlID);
+//googlID.person[0]
+
+//ds.GoogleAccess.getVerifyTokenSecret();
+
+lyleGS = lyle.GlassSettings[0];
+
+//lyleGS.listSubscriptions();
+
+//log = ds.GlassLog.createEntity(); 
+//log.request = "add subscription";
+//log.response = lyleGS.subscribeToMirrorApi();
+lyleGS.deleteSubscriptionToMirrorApi("timeline");
+//log.orig = "test script";
+//log.save();
+//log.response;
+//ds.GlassLog.all()//.forEach( function(ent){ent.remove()}) ;
+//currentSession().unPromote();
+
+//		people = ds.Person.query("GlassSettings.name='NewUserNotifications' and GlassSettings.enabled==true");
+//		people[0].GlassSettings;
+
+
+
+//mir.postTextImageMessage("Wyatt Troxell",
+//'https://lh3.googleusercontent.com/-VESQWfAMciw/AAAAAAAAAAI/AAAAAAAAADA/BA0u0FKZCyg/photo.jpg')
+
+//mir.updateContact('GlassWakanda', 'Glass Wakanda',['https://glass.wakanda.org/lib/images/GlassWakanda_share_image.png']);
+
 
 //googlID.access_token = "expired";
 //googlID.save();
