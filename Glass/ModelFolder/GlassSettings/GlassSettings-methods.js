4c8efb4cda57778ca4bd06d7a09c06d7da7619c6
Lyle Troxell <lyle@troxell.com>
2013-10-22 15:26:24 -0700

    Revert "Removed global"
    This reverts commit 028270caf7f4128123f677b6d035b17e9c2b7742.

--- Glass/ModelFolder/GlassSettings/GlassSettings-methods.js
+++ Glass/ModelFolder/GlassSettings/GlassSettings-methods.js
@@ -1,25 +1,9 @@
-var Mirror = require('MirrorAPI').Mirror;
-
-var subscribeToMirrorApi = function(MirrorAPI){
-	var mirrorResponse=MirrorAPI.addSubscription();
-	log = ds.GlassLog.createEntity(); 
-	log.request = "add subscription";
-	log.response = mirrorResponse;
-	log.orig = "test script";
-	log.save();
-	if(mirrorResponse && mirrorResponse.verifyToken) {
-		delete mirrorResponse.verifyToken;
-	}
-	return mirrorResponse;
-};
-
+﻿var Mirror = require('MirrorAPI').Mirror;
+var MirrorAPI; //global - no good, but necessary to hold reference
 model.GlassSettings.events.onLoad = function()
 {
-	var MirrorAPI = new Mirror(this.owner.GoogleAccess);
-	model.GlassSettings.entityMethods.listSubscriptions = MirrorAPI.listSubscriptions;
-	model.GlassSettings.entityMethods.listContacts = MirrorAPI.listContacts;
-	model.GlassSettings.entityMethods.deleteSubscriptionToMirrorApi = MirrorAPI.deleteSubscription;
-	model.GlassSettings.entityMethods.subscribeToMirrorApi = subscribeToMirrorApi.bind(this, MirrorAPI);
+	//setting the global var in this context - will the produce a bug?
+	MirrorAPI = new Mirror(this.owner.GoogleAccess);
 }
 model.GlassSettings.events.onInit = function()
 {
@@ -49,10 +33,31 @@ model.GlassSettings.events.onRestrictingQuery = function()
 	return result;
 };
 
+model.GlassSettings.entityMethods.listSubscriptions = function(){
+	return MirrorAPI.listSubscriptions();
+};
+model.GlassSettings.entityMethods.listContacts = function(){
+	return MirrorAPI.listContacts();
+};
 model.GlassSettings.entityMethods.isSubscribed = function(){
 	var subReturn = this.listSubscriptions();
 	return (subReturn && subReturn.items && subReturn.items.length > 0);
 };
+model.GlassSettings.entityMethods.subscribeToMirrorApi = function(){
+	var mirrorResponse=MirrorAPI.addSubscription();
+	log = ds.GlassLog.createEntity(); 
+	log.request = "add subscription";
+	log.response = mirrorResponse;
+	log.orig = "test script";
+	log.save();
+	if(mirrorResponse && mirrorResponse.verifyToken) {
+		delete mirrorResponse.verifyToken;
+	}
+	return mirrorResponse;
+};
+model.GlassSettings.entityMethods.deleteSubscriptionToMirrorApi = function(subscribeID){
+	return MirrorAPI.deleteSubscription(subscribeID);
+}
 
 model.GlassSettings.entityMethods.listSubscriptions.scope ="publicOnServer";
 model.GlassSettings.entityMethods.listContacts.scope ="public";
