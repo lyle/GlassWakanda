ccd0be1c9ac9c002c99a524487c5d9a60ee9f324
Lyle Troxell <lyle@troxell.com>
2013-10-22 22:39:13 -0700

    Added logged in/out to Glass Settings

--- Glass/WebFolder/GlassSettings.waPage/scripts/index.js
+++ Glass/WebFolder/GlassSettings.waPage/scripts/index.js
@@ -7,7 +7,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	var documentEvent = {};	// @document
 	var settingEnabled = {};	// @checkbox
 // @endregion// @endlock
-var pageJustLoaded = true;
+	var pageJustLoaded = true;
 // eventHandlers// @lock
 
 	userNameEntry.change = function userNameEntry_change (event)// @startlock
@@ -24,26 +24,47 @@ var pageJustLoaded = true;
 			});
 		}
 	};// @lock
-var getContactsInfo = function(){
-	source.glassSettings.listContacts({
-		onSuccess:function(event){
-			updateContactsView(event.result);
-		},
-		onError:function(error){
-			$("#userMessage").html(error['error'][0].message).effect('highlight', {color: 'red'});
-		}
-	});
-}
-var updateContactsView = function(contactsReturn){
-	if(contactsReturn && contactsReturn.items && contactsReturn.items.length >0){
-		$('#contactsStatus').html('Your Timeline has a Glass Wakanda Contact');
-		$('#contactsStatus').append('<br /><img id="contactImage" width="250" src="' + contactsReturn.items[0].imageUrls[0] + '" />');
-	}else{
-		$('#contactsStatus').html("Your Timeline doesn't have a Glass Wakanda Contact");
-		$('#contactImage').remove();
+	showNotLoggedIn = function(){
+	  //$('#userProfile').hide();
+	  $('#welcome').hide();
+	  $('#GoogleLogin').show();
+	  //$('#introduction').show();
 	}
-}
-var getSubscriptionInfo = function(){
+	showLoggedInUser = function(user){
+	  //$('#introduction').hide();
+	  $('#welcome').show();
+	  $('#GoogleLogin').hide();
+	  //$('#userProfile').show();
+	  //$('#userProfile .uName').html(user.fullName.value);
+	  //$('#userProfile .uPic').attr('src', user.picture.value);
+	  //$('#userProfile button.logout').click(function(event){
+	  //  WAF.directory.logout({
+	  //    onSuccess: function(){
+	  //      showNotLoggedIn();
+	  //    }
+	  //  });
+	  //});
+	}
+	var getContactsInfo = function(){
+		source.glassSettings.listContacts({
+			onSuccess:function(event){
+				updateContactsView(event.result);
+			},
+			onError:function(error){
+				$("#userMessage").html(error['error'][0].message).effect('highlight', {color: 'red'});
+			}
+		});
+	}
+	var updateContactsView = function(contactsReturn){
+		if(contactsReturn && contactsReturn.items && contactsReturn.items.length >0){
+			$('#contactsStatus').html('Your Timeline has a Glass Wakanda Contact');
+			$('#contactsStatus').append('<br /><img id="contactImage" width="250" src="' + contactsReturn.items[0].imageUrls[0] + '" />');
+		}else{
+			$('#contactsStatus').html("Your Timeline doesn't have a Glass Wakanda Contact");
+			$('#contactImage').remove();
+		}
+	}
+	var getSubscriptionInfo = function(){
 			source.glassSettings.isSubscribed({
 				onSuccess:function(event){
 					//console.log(event.result)
@@ -53,14 +74,25 @@ var getSubscriptionInfo = function(){
 					$("#userMessage").html(error['error'][0].message).effect('highlight', {color: 'red'});
 				}
 			});
-}
-var updateSubscriptionView = function(show){
-	if(show) {
-		$('#subscribedStatus').html('currently subscribed to your timeline');
-	}else{
-		$('#subscribedStatus').html('not currently subscribed to your timeline');
 	}
-}
+	var updateSubscriptionView = function(show){
+		if(show) {
+			$('#subscribedStatus').html('currently subscribed to your timeline');
+		}else{
+			$('#subscribedStatus').html('not currently subscribed to your timeline');
+		}
+	};
+	GoogleLogin.click = function GoogleLogin_click (event)// @startlock
+	{// @endlock
+	  ds.GoogleAccess.getGlassAuthenticationURL({
+	    'onSuccess': function (event) {
+	      document.location.href = event.result;
+	    },  
+	    'onError': function (event) {
+	      console.log("Error, server did not return getVisitorAuthenticationURL");
+	    }
+	  });
+	};
 	glassSettingsEvent.onCurrentElementChange = function glassSettingsEvent_onCurrentElementChange (event)// @startlock
 	{// @endlock
 		
@@ -69,14 +101,31 @@ var updateSubscriptionView = function(show){
 			pageJustLoaded = false;
 			getSubscriptionInfo();
 			getContactsInfo();
+			$('#toVanityURL').click(
+				function(){
+					document.location = ('/~' + sources.person.userName);
+				}
+			)
 		}
 	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
 		
-		//console.log('just loaded');
-	};// @lock
+		showNotLoggedIn();
+		ds.Person.getCurrentPerson({
+			onSuccess:function(event){
+				if (event.result && event.result.error){
+					console.log(event.result.errorMessage);
+				}else{
+					sources.person.addEntity(event.result);
+					showLoggedInUser(event.result);
+				}
+			},onError:function(event){
+				console.log("error");
+			}
+		});
+  	};// @lock
 
 	settingEnabled.click = function settingEnabled_click (event)// @startlock
 	{// @endlock
@@ -98,6 +147,7 @@ var updateSubscriptionView = function(show){
 	WAF.addListener("userNameEntry", "change", userNameEntry.change, "WAF");
 	WAF.addListener("glassSettings", "onCurrentElementChange", glassSettingsEvent.onCurrentElementChange, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+	WAF.addListener("GoogleLogin", "click", GoogleLogin.click, "WAF");
 	WAF.addListener("settingEnabled", "click", settingEnabled.click, "WAF");
 // @endregion
 };// @endlock
