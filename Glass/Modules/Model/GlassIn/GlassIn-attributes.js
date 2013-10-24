9fe1de14de49c50f317d7cd9f6f2b3b8905160af
Lyle Troxell <lyle@troxell.com>
2013-10-23 17:07:38 -0700

    I meant String, a type of text. :)

--- Glass/Modules/Model/GlassIn/GlassIn-attributes.js
+++ Glass/Modules/Model/GlassIn/GlassIn-attributes.js
@@ -6,9 +6,9 @@ GlassIn.googleAccount = new Attribute("relatedEntity", "GoogleAccess", "GoogleAc
 //note from Mirror Notification user_token should be googleAccount;
 
 
-GlassIn.itemId = new Attribute("storage", "text");
+GlassIn.itemId = new Attribute("storage", "string");
 GlassIn.processed_at = new Attribute("storage", "date");
-GlassIn.notificationBody = new Attribute("storage", "text");
+GlassIn.notificationBody = new Attribute("storage", "string");
 
 var primeDates = require("Model/dateAttributes/created_updated.js");
 GlassIn.create_at = primeDates.create_at;
